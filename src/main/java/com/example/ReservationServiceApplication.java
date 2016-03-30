d162a36db19c7ea78efcf8cd4e970a7a9a60ac35
Gustavo Andrade <gustavoandrade82@gmail.com>
2016-03-29 21:58:36 -0300

    Initial commit

---
+++ src/main/java/com/example/ReservationServiceApplication.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${message}")
+	private String message;
+
+	@RequestMapping("/message")
+	String message() {
+		return this.message;
+	}
+}
+
